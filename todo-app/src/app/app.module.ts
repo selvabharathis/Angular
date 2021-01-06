a0ea4a654230f0f13884987dce778ffd8cd02c9f
selvabharathi <selvabharathi327@gmail.com>
2021-01-07 00:01:14 +0530

    assignment added

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from "@angular/forms"
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { HeaderComponent } from './layout/header/header.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome"
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HeaderComponent } from './layout/header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todo-app/src/app/components/todo-form/todo-form.component.ts
+++ todo-app/src/app/components/todo-form/todo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from "./../../model/Todo"
+import {v4 as uuidv4} from "uuid"
+import {TodoService} from "./../../service/todo.service"
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFormComponent implements OnInit {
 
-  constructor() { }
+  todoTitle: string;
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+  }
+
+  handleAdd(){
+    const newTodo:Todo = {
+      id: uuidv4(),
+      title: this.todoTitle,
+      isComplete: false,
+      date: new Date
+    }
+    this.todoService.addTodo(newTodo);
+    this.todoTitle = "";
   }
 
 }
