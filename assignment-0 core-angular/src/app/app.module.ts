8cae669290055b07aff4e35f1a10f525b1b6f546
selvabharathi <selvabharathi327@gmail.com>
2024-01-29 14:29:34 +0530

    initial load

--- assignment-0 core-angular/src/app/app.module.ts
+++ assignment-0 core-angular/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core"
 import {BrowserModule} from "@angular/platform-browser"
 import { AppComponent } from "./app.component"
-import { CardComponent } from "./card.component"
+import { CardComponent } from "./card.component";
+import { CarComponent } from './car/car.component'
 
 @NgModule({
     imports : [BrowserModule],
-    declarations:[AppComponent,CardComponent],
+    declarations:[AppComponent,CardComponent, CarComponent],
     bootstrap:[AppComponent]
 })
 export class AppModule{}
